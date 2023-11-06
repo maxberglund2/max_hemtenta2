e16ec62c3b7aaef73318a3d03103845498966dea
Greed <106820018+maxberglund2@users.noreply.github.com>
2023-11-06 11:33:35 +0100

    files
    basic structure

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meny</title>
+</head>
+<body>
+    <a href="http://localhost/max_hemtenta2/add.php">Lägg till produkt</a>
+    <a href="http://localhost/max_hemtenta2/read.php">Se alla produkter</a>
+    <a href="http://localhost/max_hemtenta2/edit.php">Ändra pris/bild på produkt</a>
+    <a href="http://localhost/max_hemtenta2/remove.php">Ta bort produkt</a>
+</body>
+</html>
