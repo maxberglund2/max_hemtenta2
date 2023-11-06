8b291e477fe412ccb6b20edfe0d71b5b04ad51bc
Greed <106820018+maxberglund2@users.noreply.github.com>
2023-11-06 16:26:48 +0100

    clean code

--- funktioner/edit.php
+++ funktioner/edit.php
@@ -17,8 +17,6 @@
                     $sql = "SELECT * FROM products";
                     $result = $conn->query($sql);
 
-                    
-
                     if ($result->num_rows > 0) {
                         // en while loop som går igenom tabblen
                         while($row = $result->fetch_assoc()) {
--- funktioner/read.php
+++ funktioner/read.php
@@ -9,25 +9,41 @@
     </style>
 </head>
 <body>
-    <?php
-        include 'connection.php';
+    <div class='con'>
+        <table>
+            <tr><th colspan='4'>Alla Produkter</th></tr>
+            <?php
+                include 'connection.php';
 
-        $sql = "SELECT * FROM products";
-        $result = $conn->query($sql);
+                $sql = "SELECT * FROM products";
+                $result = $conn->query($sql);
 
-        $table = "<div class='con'><table><tr><th colspan='4'>Products</th></tr>";
+                $table = "<div class='con'><table><tr><th colspan='4'>Products</th></tr>";
 
-        if ($result->num_rows > 0) {
-            // en while loop som går igenom tabblen
-            while($row = $result->fetch_assoc()) {
-                $table .= '<tr><td>' . $row["name"] . '</td><td>' . $row["description"] . '</td><td>' . $row["price"] . '</td><td><img src="../' . $row["image"] . '" alt="' . $row["name"] . '"></td></tr>';
-            }
-            echo $table .= '</table><div class="btnMenu"><a href="http://localhost/max_hemtenta2">Användargränssnitt</a><a href="http://localhost/max_hemtenta2/funktioner/add.php">Lägg till produkt</a><a href="http://localhost/max_hemtenta2/funktioner/edit.php">Ändra pris/bild på produkt</a><a href="http://localhost/max_hemtenta2/funktioner/remove.php">Ta bort produkt</a></div><div>';
-        } else {
-            echo "0 results";
-        }
+                if ($result->num_rows > 0) {
+                    // en while loop som går igenom tabblen
+                    while($row = $result->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>" . $row["name"] . "</td>";
+                        echo "<td>" . $row["description"] . "</td>";
+                        echo "<td>" . $row["price"] . "kr</td>";
+                        echo "<td><img src='../" . $row["image"] . "' alt='" . $row["name"] . "'></td>";
+                        echo "</tr>";
+                    }
+                } else {
+                    echo "0 results";
+                }
 
-        $conn->close();
-    ?>
+                $conn->close();
+            ?>
+        </table>
+        <div class="btnMenu">
+            <a href="http://localhost/max_hemtenta2">Användargränssnitt</a>
+            <a href="http://localhost/max_hemtenta2/funktioner/add.php">Lägg till produkt</a>
+            <a href="http://localhost/max_hemtenta2/funktioner/read.php" class="activeBtn">Se alla produkter</a>
+            <a href="http://localhost/max_hemtenta2/funktioner/edit.php">Ändra pris/bild på produkt</a>
+            <a href="http://localhost/max_hemtenta2/funktioner/remove.php">Ta bort produkt</a>
+        </div>
+    <div>
 </body>
 </html>
